f3c6d9f211ea35dc81a76be8040cc9f218823ff7
Jino Lacson <jino.lacson@boom.camp>
2020-04-24 09:28:21 +0800

    Update app.blade.php

--- relationship/resources/views/app.blade.php
+++ relationship/resources/views/app.blade.php
@@ -4,7 +4,7 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" value="{{ csrf_token() }}"/>
-    <title>Laravel & Vue CRUD Single Page Application (SPA) Tutorial - MyNotePaper</title>
+    <title>Laravel & Vue CRUD Single Page Application (SPA) Tutorial</title>
     <link href="https://fonts.googleapis.com/css?family=Nunito:200,600" rel="stylesheet" type="text/css">
     <link href="{{ mix('css/app.css') }}" type="text/css" rel="stylesheet"/>
     <style>
